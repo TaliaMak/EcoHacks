7742fdd3c87d7e1eedd3288d2bb7fa6cdd1b1486
camrynkim <cok9bhr@virginia.edu>
2023-04-15 11:51:56 -0400

    added models and fixed settings

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User  # https://docs.djangoproject.com/en/4.2/topics/auth/default/
+
 
 # Create your models here.
+class User(User):
+    pass
