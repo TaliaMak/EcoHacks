e01e3dfc51cb472b9ffd1e33603e1092dd13a4a3
TaliaMak <nvm6nf@virginia.edu>
2023-04-15 12:51:03 -0400

    added models to models.py

--- myapp/models.py
+++ myapp/models.py
@@ -5,3 +5,37 @@ from django.contrib.auth.models import User  # https://docs.djangoproject.com/en
 # Create your models here.
 class User(User):
     pass
+
+class Transportation(models.Model):
+   TYPE = (
+       'bus',
+       'car',
+       'bike',
+       'electric scooter/bike',
+       'walk',
+   )
+   carpool = models.BooleanField(default=False)
+   secondhand = models.BooleanField(default=False)
+   type = models.CharField(max_length=1, choices=TYPE)
+   
+class Food(models.Model):
+   vegan = models.BooleanField(default=False)
+   vegetarian = models.BooleanField(default=False)
+   organic = models.BooleanField(default=False)
+   MEAT_TYPE = (
+       'pork',
+       'poultry',
+       'beef',
+       'seafood'
+   )
+   meat_type = models.CharField(max_length=1, choices=MEAT_TYPE)
+   
+class MaterialGoods(models.Model):
+    ITEM = (
+        'water bottle'
+        'reusable bag'
+        'technology'
+    )
+    item = models.CharField(max_length=1, choices=ITEM)
+    months_owned = models.IntegerField()
+    
